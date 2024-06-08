1b234ef1a6e53aeb37a8fc7fb7022a43d36798d4
Lucas <trabalholucas93@gmail.com>
2024-06-08 13:33:23 -0300

    Inserido a controladora MedidorController.

---
+++ src/main/java/devandroid/lucas/appcontrolemedidores/controller/MedidorController.java
@@ -0,0 +1,14 @@
+package devandroid.lucas.appcontrolemedidores.controller;
+
+import android.content.Context;
+
+import devandroid.lucas.appcontrolemedidores.database.ControleMedidorDB;
+import devandroid.lucas.appcontrolemedidores.view.ControleMedidorActivity;
+
+public class MedidorController extends ControleMedidorDB {
+
+
+    public MedidorController(ControleMedidorActivity activity) {
+        super(activity);
+    }
+}
